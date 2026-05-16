ae410bbfcea369187ac367bd3f18874c08b7c169
Eduardomesut <eduardomesut@gmail.com>
2026-05-17 01:39:25 +0200

    Implement services and dtos

---
+++ src/main/java/com/msfootballmarket/demo/entity/PlayerInformationOutput.java
@@ -0,0 +1,5 @@
+package com.msfootballmarket.demo.entity;
+
+public class PlayerInformationOutput {
+    
+}
---
+++ src/main/java/com/msfootballmarket/demo/service/managePlayerMarket.java
@@ -0,0 +1,5 @@
+package com.msfootballmarket.demo.service;
+
+public class managePlayerMarket {
+    
+}
